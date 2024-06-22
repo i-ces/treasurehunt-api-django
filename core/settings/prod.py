517de15e5759fed9faa314e7a38aefcea453287d
amritgiri <amritgiri5813@gmail.com>
2024-06-22 14:15:01 +0545

    Create level model

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -5,7 +5,7 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
+        "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
