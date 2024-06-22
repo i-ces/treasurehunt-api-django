75b066c216936df34063fabbafcbea59a187eb3b
Shishir Rijal <shishirrijal@Shishirs-Laptop.local>
2024-06-22 23:23:36 +0545

    resolve conflict

--- apps/urls.py
+++ apps/urls.py
@@ -3,18 +3,19 @@ from rest_framework import routers
 
 from .views import RiddleViewSet, RiddleByLevelAPIView
 from .views import RiddleListCreateAPIView
+from .views import RiddleListCreateAPIView, LoginApiView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 router = routers.DefaultRouter()
-router.register(r"riddle", RiddleViewSet, basename="level")
-router.register(r"level/(?P<level>\d+)", RiddleByLevelAPIView,
-                basename="riddles-by-level")
+router.register(r"riddles", RiddleListCreateAPIView, basename="riddles")
+router.register(r"", LoginApiView, basename="login")
 
+# router.register(r')
 urlpatterns = [
     path("api/", include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
