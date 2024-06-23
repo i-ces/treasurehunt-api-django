80feae0ab4c146ebc2163d2d49b1bf73da2f9844
amritgiri <amritgiri5813@gmail.com>
2024-06-23 22:36:55 +0545

    Create a api that filters the level when trap count is greater than 2 and also make level api to display the riddles that contains available instance true

--- apps/models/level.py
+++ apps/models/level.py
@@ -3,6 +3,7 @@ from django.db import models
 class Level(models.Model):
     number = models.IntegerField(unique=True)
     name = models.CharField(max_length=500)
+    trap_count = models.IntegerField(default=0)
 
     def __str__(self):
         return f"Level {self.number}: {self.name}"
